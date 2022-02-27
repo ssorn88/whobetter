14749785263f68f400c400c96baec44bc2e030a6
unknown <es4402@korea.ac.kr>
2022-02-27 21:50:47 +0900

    homeurl

--- promiseapp/urls.py
+++ promiseapp/urls.py
@@ -1,4 +1,10 @@
+from django.urls import path
+
+from promiseapp.views import home
+
+app_name = "promiseapp"
 
 urlpatterns = [
+    path('home/', home, name='home'),
 
 ]
--- promiseapp/views.py
+++ promiseapp/views.py
@@ -1,3 +1,10 @@
+from django.http import HttpResponseRedirect, HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
+from django.urls import reverse
+from django.utils.translation import template
+
+
+def home(request):
+    return render(request,'promiseapp/home.html')
