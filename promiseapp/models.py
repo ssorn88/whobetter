5e4f9f06df9ce4f79e0a68cd6e6e0e39d9ebba8a
unknown <es4402@korea.ac.kr>
2022-02-27 16:38:57 +0900

    initail

---
+++ promiseapp/models.py
@@ -0,0 +1,22 @@
+from django.db import models
+
+
+# Create your models here.
+
+class Promise(models.Model):
+    name = models.CharField(max_length=10, null=True)
+    election = models.CharField(max_length=20, null=True)
+    party = models.CharField(max_length=20, null=True)
+    keyword = models.CharField(max_length=20, null=True)
+
+    corona = models.ForeignKey
+    realty = models.ForeignKey
+    military = models.ForeignKey
+    politics = models.ForeignKey
+    economy = models.ForeignKey
+    construction = models.ForeignKey
+    environ = models.ForeignKey
+    science = models.ForeignKey
+    culture = models.ForeignKey
+    edu = models.ForeignKey
+
