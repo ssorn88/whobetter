ad60440bddbb35e9dcb32d987ad22ccd03219858
isdiscodead <isdiscodead@korea.ac.kr>
2022-03-01 19:44:31 +0900

    add json

--- promiseapp/views.py
+++ promiseapp/views.py
@@ -14,6 +14,7 @@ def home(request):
 
 class CandidateListView(ListView):
     model = DepartCon
+    context_object_name = "candidate_list"
     template_name = 'promiseapp/compare.html'
 
     def get_context_data(self, *, object_list=None, **kwargs):
@@ -31,3 +32,5 @@ class CandidateListView(ListView):
             "depart_json": depart_json,
             "candidate_json": candidate_json
         }
+
+        return context
