362c0eb56c76922babcac04dd6db1cdc95669efc
owlwith <owlwithrain@gmail.com>
2022-03-01 21:16:43 +0900

    database insert image

--- promiseapp/models.py
+++ promiseapp/models.py
@@ -9,6 +9,7 @@ from django.db import models
 class Candidate(models.Model):
     name = models.CharField(max_length=10, unique=True, null=False)
     election = models.CharField(max_length=10, null=True)
+    image = models.ImageField(upload_to='promise/', null=True)
     party = models.CharField(max_length=20, null=True)
     slogan = models.CharField(max_length=50, null=True)
     keyword = models.TextField(null=True)
