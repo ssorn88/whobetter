1cc2a9a5bb09034f88a9583f996b6acb01fd904d
owlwith <owlwithrain@gmail.com>
2022-03-01 15:40:40 +0900

    database restore 1540

--- promiseapp/views.py
+++ promiseapp/views.py
@@ -2,10 +2,16 @@ from django.shortcuts import render
 
 
 # Create your views here.
+from django.views.generic import ListView
+
+from promiseapp.models import DepartCon
+
 
 def home(request):
     return render(request, 'promiseapp/home.html')
 
 
-def compare(request):
-    return render(request, 'promiseapp/compare.html')
+class CandidateListView(ListView):
+    model = DepartCon
+    context_object_name = 'candidate_list'
+    template_name = 'promiseapp/compare.html'
