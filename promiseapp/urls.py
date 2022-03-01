fc4e4eac98c780b0517495bbd11da403ab2f6215
unknown <es4402@korea.ac.kr>
2022-03-01 16:36:54 +0900

    urlfileedit

--- promiseapp/urls.py
+++ promiseapp/urls.py
@@ -1,18 +1,11 @@
 from django.urls import path
 
-<<<<<<< HEAD
 
 from promiseapp.views import home, CandidateListView
-=======
-from promiseapp.views import CandidateListView
->>>>>>> develop
 
 app_name = "promiseapp"
 
 urlpatterns = [
-<<<<<<< HEAD
     path('home/', home, name='home'),
-=======
->>>>>>> develop
     path('compare/', CandidateListView.as_view(), name='compare')
 ]
